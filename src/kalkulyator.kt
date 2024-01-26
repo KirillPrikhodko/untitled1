684186bd6b607638ca3d9731b8826f9bb42c14aa
student <kirill.prikhodko@outlook.com>
2024-01-26 16:40:44 +0300

    when calculate

--- src/kalkulyator.kt
+++ src/kalkulyator.kt
@@ -2,6 +2,9 @@ fun main (){
     var x = readLine()?.toDoubleOrNull()!!
     var b = readLine()!!
     var y = readLine()!!.toDouble()
+    calculate(x, y, b)
+}
+fun calculate(x:Double, y:Double, b:String){
     when(b){
         "+"-> println("$x + $y = ${x+y}")
         "-"-> println("$x - $y = ${x-y}")
