7b3c54c32a8d52fb33ffba348a264e1acbbe40ba
student <kirill.prikhodko@outlook.com>
2024-01-30 12:41:53 +0300

    task

--- src/kalkulyator.kt
+++ src/kalkulyator.kt
@@ -1,15 +1,26 @@
-fun main (){
-    var x = readLine()?.toDoubleOrNull()!!
-    var b = readLine()!!
-    var y = readLine()!!.toDouble()
-    calculate(x, y, b)
+fun task1() {
+    while (true) {
+        try {
+            var x = readLine()?.toDoubleOrNull()!!
+            var b = readLine()!!
+            var y = readLine()!!.toDouble()
+            calculate(x, y, b)
+        } catch(e:Exception){
+            println("Ошибка")
+        }
+    }
 }
-fun calculate(x:Double, y:Double, b:String){
-    when(b){
-        "+"-> println("$x + $y = ${x+y}")
-        "-"-> println("$x - $y = ${x-y}")
-        "*"-> println("$x * $y = ${x*y}")
-        "/"-> println("$x / $y = ${x.toFloat()/y}")
-        else->print("Ошибка")
+
+fun main() {
+    task1()
+}
+
+fun calculate(x: Double, y: Double, b: String) {
+    when (b) {
+        "+" -> println("$x + $y = ${x + y}")
+        "-" -> println("$x - $y = ${x - y}")
+        "*" -> println("$x * $y = ${x * y}")
+        "/" -> println("$x / $y = ${x.toFloat() / y}")
+        else -> print("Ошибка")
     }
 }
